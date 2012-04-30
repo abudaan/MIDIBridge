8acc6dffad41d0dc0738ead6fe2898f39315a908
abudaan <abudaan@gmail.com>
2012-04-30 18:02:33 +0200

    updated the basic example and the README

--- README.md
+++ README.md
@@ -14,11 +14,17 @@ The only files you need to get started are:
 /lib/MidiBridge.js is the non-minified version of /lib/midibridge-0.6.0.min.js
 
 
-index.html shows you how you embed the MIDIBridge in your html page; you can use this basic example as a starting point of your own code.
+The MIDIBridge is written in native Javascript, so you can use it conflict-free with any Javascript framework. If you use a framework, you have to embed it in your HTML file before you embed the MIDIBridge. 
 
-The MIDIBridge is written in native Javascript, so you can use it conflict-free with any Javascript framework. If you use a framework, you have to embed it before you embed the MIDIBridge.
+The examples in the /examples folder show you how to embed the MIDIBridge in a HTML page. 
 
-In the /examples folder you will find more advanced examples. These examples also use MIDIBridge plugins, I have provided 3 plugins that makes it easier to develop your own projects and keeping your code as clean as possible.
+The basic example shows a basic usage of MIDIAccess, you can use this as a starting point of your own code.
+
+The other examples are a bit more advanced, these examples also use MIDIBridge plugins. I have provided 3 plugins that makes it easier to develop your own projects and keeping your code as clean as possible.
+
+
+The documentation is in progress. See: https://github.com/abudaan/MIDIBridge/wiki
 
 
 If you come across bugs or issues, or if you have feature requests or other questions, file an issue on Github, or email me at daniel@abumarkub.net
+
--- examples/js/basic-example.js
+++ examples/js/basic-example.js
@@ -9,7 +9,7 @@
  *  if the first MIDI input is a keyboard and the firt MIDI output is a synthesizer, or is connected to a synthesizer,
  *  you should be able to hear what you play on your keyboard.
  *  
- *  if not, try the setup-connections example in the examples folder
+ *  if not, try the setup-connections example
  * 
  *  dependecies:
  *  - MIDIBridge.js
@@ -18,21 +18,17 @@
 
 window.addEventListener('load', function() {
     
-    var content = document.getElementById("content"),
-    messages = document.createElement("div");
-    messages.setAttribute("id","messages");
-    
+    var devices = document.getElementById("devices"),
+    messages = document.getElementById("messages");    
 
     midiBridge.init(function(MIDIAccess){
         
         var input = MIDIAccess.getInput(MIDIAccess.enumerateInputs()[0]);
-        content.innerHTML += "<div class='device'><span class='device-type'>input:</span><span class='device-name'>" + input.deviceName + "</span></div>";        
+        devices.innerHTML += "<div class='device'><span class='device-type'>input: </span><span class='device-name'>" + input.deviceName + "</span></div>";        
             
         var output = MIDIAccess.getOutput(MIDIAccess.enumerateOutputs()[0]);
-        content.innerHTML += "<div class='device'><span class='device-type'>output:</span><span class='device-name'>" + output.deviceName + "</span></div>";                                   
+        devices.innerHTML += "<div class='device'><span class='device-type'>output: </span><span class='device-name'>" + output.deviceName + "</span></div>";                                   
         
-        content.appendChild(messages);
-
         input.addEventListener("midimessage",function(e){
             messages.innerHTML += "<div class='message'>" + e.toString() + "</div>";
             output.sendMIDIMessage(e);
